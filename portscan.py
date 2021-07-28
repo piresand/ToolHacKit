6eb927cb2ca59f3f4894d2ab6feb95395ada5001
Oldboy <andrepires.corporativo@gmail.com>
2021-07-27 21:31:59 -0400

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,9 +1,19 @@
+# Ferramenta criada durante o treinamento de Introdução ao Hacking e Pentest da Solyd.
+# Site target: bancocn.com
+# Porta target: 80
+
 import socket
 
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente.connect(('bancocn.com', 80))
-cliente.send('hauhsas\nn')
+cliente.send('hauhsas\n\n')
 resposta = cliente.recv(1024)
 
 print resposta
+
+
+
+
+
+
 
